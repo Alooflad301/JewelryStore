f819101782ac57312b0288d46f08ae499474777b
10230047 <10230047@B316PC12>
2026-03-30 13:53:11 +0300

    авторизация и регистрация

--- JewelryStore/Pages/PageAftoriz.xaml.cs
+++ JewelryStore/Pages/PageAftoriz.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JewelryStore.AppData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,39 @@ namespace JewelryStore.Pages
         {
             InitializeComponent();
         }
+
+        private void Regtodt_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.framemain.Navigate(new PageReg());
+        }
+
+        private void DaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                try
+                {
+                    var userObj = AppData.AppConnect.model0db.User.FirstOrDefault(x => x.Login == TextLogin.Text && x.Password == PassBox.Password);
+                    if (userObj == null)
+                    {
+                        MessageBox.Show("Такого пользователя нет", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Добро пожаловать " + userObj.Login + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                        AppFrame.framemain.Navigate(new PagePrivet());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка" + ex.Message.ToString(), "Критическая ошибка приложения", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AppFrame.framemain.Navigate(new PageReg());
+        }
     }
 }
+
--- JewelryStore/Pages/PageReg.xaml.cs
+++ JewelryStore/Pages/PageReg.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using JewelryStore.AppData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -23,6 +26,60 @@ namespace JewelryStore.Pages
         public PageReg()
         {
             InitializeComponent();
+        }
+
+        private void RegBut_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (AppConnect.model0db.User.Count(x => x.Login == LoginBox.Text) > 0)
+                {
+                    MessageBox.Show("Пользователь с таким логином есть!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                if (String.IsNullOrEmpty(LoginBox.Text) ||
+                    String.IsNullOrEmpty(Password1.Password) ||
+                    String.IsNullOrWhiteSpace(Password1.Password) ||
+                    String.IsNullOrWhiteSpace(LoginBox.Text))
+                {
+                    MessageBox.Show("Не заполнены все поля!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                User userObj = new User()
+                {
+                    Login = LoginBox.Text,
+                    Password = Password1.Password,
+                    Phone = TelevonBox.Text,
+                    Email = EmailBox.Text,
+                    IdRole = 1
+                    
+                };
+                AppConnect.model0db.User.Add(userObj);
+                AppConnect.model0db.SaveChanges();
+                MessageBox.Show("Данные успешно добавлены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                AppData.AppFrame.framemain.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при добавлении данных!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Password2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (Password1.Password != Password2.Password)
+            {
+                RegBut.IsEnabled = false;
+                Password2.Background = Brushes.LightCoral;
+                Password2.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                RegBut.IsEnabled = true;
+                Password2.Background = Brushes.LightGreen;
+                Password2.BorderBrush = Brushes.Green;
+            }
         }
     }
 }
